fcbcd8fd2f61e0f6e0d0926206f03a3cb191c452
Mickaël Fontaine <m.fontaine@linkbynet.com>
2018-12-08 13:13:30 +0100

    Add socketIO

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 import connexion
+from flask_socketio import SocketIO
 import config
 
 # import main config
@@ -10,6 +11,17 @@ app = connexion.App(__name__, specification_dir='./')
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
 
+
+# SocketIO related
+flask_app = app.app
+socketio = SocketIO(flask_app)
+
+
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(host=config['host'], port=config['port'], debug=True)
+    socketio.run(
+        flask_app,
+        host=config['host'],
+        port=config['port'],
+        debug=True
+    )
