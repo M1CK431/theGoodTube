845a8325a1e546da6bac0cb84356e7f2fd08a8d4
Mickaël Fontaine <m.fontaine@linkbynet.com>
2018-12-25 11:50:52 +0100

    Embed a test client for socketIO

--- src/server.py
+++ src/server.py
@@ -1,6 +1,7 @@
 import connexion
 from flask_socketio import SocketIO
 import config
+import sys
 
 # import main config
 config = config.get_config()['main']
@@ -17,6 +18,18 @@ flask_app = app.app
 socketio = SocketIO(flask_app)
 
 
+@app.route("/socketio", methods=["GET"])
+def socketioclient():
+    with open(sys.path[0] + '/socketIO.html', 'r') as file:
+        return file.read()
+
+
+@app.route("/socketiojs", methods=["GET"])
+def socketiojsclient():
+    with open(sys.path[0] + '/socket.io.slim.dev.js', 'r') as file:
+        return file.read()
+
+
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
     socketio.run(
@@ -25,3 +38,5 @@ if __name__ == '__main__':
         port=config['port'],
         debug=True
     )
+
+socketio.emit('connect', "connected")
