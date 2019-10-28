0d7c5a6ea87cf80f0573ec3d5702cdba1fa8855c
M1CK431 <mickael@delight-data.com>
2019-10-28 23:33:58 +0100

    Limit progress events rate

--- thegoodtube/events_queue.py
+++ thegoodtube/events_queue.py
@@ -1,15 +1,25 @@
 from multiprocessing import SimpleQueue
 from threading import Thread
 from . import socketio
+from datetime import datetime, timedelta
 
 events_queue = SimpleQueue()
 
 
 def events_queue_thread():
+    interval = timedelta(milliseconds=500)
+    last_progress_event_datetime = datetime.now()
     while True:
         event = events_queue.get()
-        socketio.emit(event["name"], event["payload"])
-
+        if (
+            event["name"] == "progress"
+            and event["payload"]["progress"] == "downloading"
+        ):
+            if datetime.now() > last_progress_event_datetime + interval:
+                socketio.emit(event["name"], event["payload"])
+                last_progress_event_datetime = datetime.now()
+        else:
+            socketio.emit(event["name"], event["payload"])
 
 events_thread = Thread(target=events_queue_thread, daemon=True)
 events_thread.start()
